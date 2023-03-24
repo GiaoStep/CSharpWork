5f3ef0335c642504e9e30cb37dc184992ecb26b9
GiaoStep <121810051+GiaoStep@users.noreply.github.com>
2023-03-24 23:48:45 +0800

    Add files via upload

---
+++ work3/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,62 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace AlarmClock
+{
+    public delegate void TickEventHandler(object sender, EventArgs e);
+    public delegate void AlarmEventHandler(object sender, EventArgs e);
+
+    public class AlarmClock
+    {
+        private int hour;
+        private int minute;
+        private int second;
+        private bool alarmOn;
+
+        public event TickEventHandler Tick;
+        public event AlarmEventHandler Alarm;
+
+        public AlarmClock(int hour, int minute, int second, bool alarmOn)
+        {
+            this.hour = hour;
+            this.minute = minute;
+            this.second = second;
+            this.alarmOn = alarmOn;
+        }
+
+        public void Run()
+        {
+            while (true)
+            {
+                System.Threading.Thread.Sleep(1000);
+                Tick?.Invoke(this, EventArgs.Empty);
+
+                if (DateTime.Now.Hour == hour && DateTime.Now.Minute == minute && DateTime.Now.Second == second && alarmOn)
+                {
+                    Alarm?.Invoke(this, EventArgs.Empty);
+                }
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AlarmClock clock = new AlarmClock(23, 18, 59, true);
+            clock.Tick += OnTick;
+            clock.Alarm += OnAlarm;
+            clock.Run();
+        }
+
+        static void OnTick(object sender, EventArgs e)
+        {
+            Console.WriteLine("Tick...");
+        }
+
+        static void OnAlarm(object sender, EventArgs e)
+        {
+            Console.WriteLine("Alarm!!");
+        }
+    }
+}
---
+++ work3/ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,109 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+public class Node<T>
+{
+    public Node<T> Next { get; set; }
+    public T Data { get; set; }
+
+    public Node(T t)
+    {
+        Next = null;
+        Data = t;
+    }
+}
+
+public class GenericList<T>
+{
+    private Node<T> head;
+    private Node<T> tail;
+
+    public GenericList()
+    {
+        tail = head = null;
+    }
+
+    public Node<T> Head
+    {
+        get => head;
+    }
+
+    public void Add(T t)
+    {
+        Node<T> n = new Node<T>(t);
+
+        if (tail == null)
+        {
+            head = tail = n;
+        }
+        else
+        {
+            tail.Next = n;
+            tail = n;
+        }
+    }
+
+    public void ForEach(Action<T> action)
+    {
+        for (Node<T> node = head; node != null; node = node.Next)
+        {
+            action(node.Data);
+        }
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // 整型List
+        GenericList<int> intList = new GenericList<int>();
+        for (int x = 0; x < 10; x++)
+        {
+            intList.Add(x);
+        }
+
+        // 打印链表元素
+        Console.Write("intList: ");
+        intList.ForEach((n) => Console.Write($"{n} "));
+
+        // 求最大值
+        int max = int.MinValue;
+        intList.ForEach((n) =>
+        {
+            if (n > max)
+            {
+                max = n;
+            }
+        });
+        Console.WriteLine($"\nintList最大值: {max}");
+
+        // 求最小值
+        int min = int.MaxValue;
+        intList.ForEach((n) =>
+        {
+            if (n < min)
+            {
+                min = n;
+            }
+        });
+        Console.WriteLine($"intList最小值: {min}");
+
+        // 求和
+        int sum = 0;
+        intList.ForEach((n) => sum += n);
+        Console.WriteLine($"intList求和: {sum}");
+
+        // 字符串型List
+        GenericList<string> strList = new GenericList<string>();
+        for (int x = 0; x < 10; x++)
+        {
+            strList.Add($"str{x}");
+        }
+
+        // 打印链表元素
+        Console.Write("strList: ");
+        strList.ForEach((n) => Console.Write($"{n} "));
+    }
+}
+
